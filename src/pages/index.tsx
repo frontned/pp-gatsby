fc6c2b95204d6c445ac925e012584b91cfc305d7
unknown <nedass@wix.com>
2021-10-08 14:41:48 +0300

    WIP

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <div>asdadassad</div>
+    </main>
+  )
+}
+
+export default IndexPage
